f65d06dcfa0be622cab431ebdac577ffd2013bf3
Kit Knox <kit@rootshell.com>
2026-04-01 07:47:54 -0700

    Add rootshell cask v1.0.2-61

---
+++ Casks/rootshell.rb
@@ -0,0 +1,21 @@
+cask "rootshell" do
+  version "1.0.2,61"
+  sha256 "cbb491bbc2cbd44c4480db2a3ed02abbc1faa039e320ca35c9d1717886ebd367"
+
+  url "https://www.rootshell.com/downloads/rootshell-macos-#{version.before_comma}-#{version.after_comma}.tar.xz"
+  name "rootshell"
+  desc "Local Terminal, SSH"
+  homepage "https://www.rootshell.com/"
+
+  auto_updates true
+
+  app "rootshell.app"
+
+  zap trash: [
+    "~/Library/Application Support/com.kk2.rootshell",
+    "~/Library/Caches/com.kk2.rootshell",
+    "~/Library/HTTPStorages/com.kk2.rootshell",
+    "~/Library/Preferences/com.kk2.rootshell.plist",
+    "~/Library/Saved Application State/com.kk2.rootshell.savedState",
+  ]
+end

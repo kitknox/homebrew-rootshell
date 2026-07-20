d6010208085866fb40980c5d811faadc342266be
Kit Knox <kit@rootshell.com>
2026-07-20 14:16:20 -0700

    Add rootshell cask v1.0.9-124

--- Casks/rootshell.rb
+++ Casks/rootshell.rb
@@ -1,6 +1,6 @@
 cask "rootshell" do
-  version "1.0.9,123"
-  sha256 "4eb58f8ec0ba8116daa145324cf3c58b2bf0dc8621f0cc3de3e33d7cc1ed0a7d"
+  version "1.0.9,124"
+  sha256 "a0b930e29d21503b6cf4e1c4892b99ccfa72c8ca9cfa67550219d7b9e88e0cfd"
 
   url "https://www.rootshell.com/downloads/rootshell-macos-#{version.before_comma}-#{version.after_comma}.tar.xz"
   name "rootshell"
